6c769b846605a5858c87876b04102f271f04b5cf
Firestar311 <slaterandrew0398@gmail.com>
2025-11-27 11:30:38 -0600

    Added 1.21.11 to MinecraftVersion and added plugin detection. Also moved the UNDEFINED constant below the versions

--- common/src/main/java/com/stardevllc/smcversion/MinecraftVersion.java
+++ common/src/main/java/com/stardevllc/smcversion/MinecraftVersion.java
@@ -1,12 +1,11 @@
 package com.stardevllc.smcversion;
 
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
+import java.lang.reflect.*;
 
 public enum MinecraftVersion {
-    UNDEFINED, 
     v1_8, v1_8_1, v1_8_2, v1_8_3, v1_8_4, v1_8_5, v1_8_6, v1_8_7, v1_8_8, v1_8_9,
     v1_9, v1_9_1, v1_9_2, v1_9_3, v1_9_4, 
     v1_10, v1_10_1, v1_10_2, 
@@ -20,7 +19,9 @@ public enum MinecraftVersion {
     v1_18, v1_18_1, v1_18_2, 
     v1_19, v1_19_1, v1_19_2, v1_19_3, v1_19_4, 
     v1_20, v1_20_1, v1_20_2, v1_20_3, v1_20_4, v1_20_5, v1_20_6, 
-    v1_21, v1_21_1, v1_21_2, v1_21_3, v1_21_4, v1_21_5, v1_21_6, v1_21_7, v1_21_8, v1_21_9, v1_21_10, v1_21_11;
+    v1_21, v1_21_1, v1_21_2, v1_21_3, v1_21_4, v1_21_5, v1_21_6, v1_21_7, v1_21_8, v1_21_9, v1_21_10, v1_21_11,
+    
+    UNDEFINED;
     
     static {
         //Reflectively add a string converter to the StringConverters to prevent having to have a dependency
@@ -38,13 +39,29 @@ public enum MinecraftVersion {
     public static final MinecraftVersion CURRENT_VERSION = getVersion();
     
     private static MinecraftVersion getVersion() {
+        JavaPlugin plugin = null;
+        try {
+            Class<?> pluginClassLoader = Class.forName("org.bukkit.plugin.java.PluginClassLoader");
+            ClassLoader classLoader = MinecraftVersion.class.getClassLoader();
+            if (pluginClassLoader.isInstance(classLoader)) {
+                Field pluginField = pluginClassLoader.getDeclaredField("plugin");
+                pluginField.setAccessible(true);
+                plugin = (JavaPlugin) pluginField.get(classLoader);
+            }
+        } catch (Throwable e) {}
+        
+        if (plugin == null) {
+            Bukkit.getLogger().severe("[SMCVersion] Could not determine the plugin that holds the MinecraftVersion enum");
+            return UNDEFINED;
+        }
+        
         try {
             String spigotVersion = Bukkit.getServer().getVersion();
             String[] split = spigotVersion.split("MC: ");
             
             return valueOf("v" + split[1].replace(")", "").replace(".", "_"));
         } catch (Exception e) {
-            System.out.println("Incompatible version detected. Please ensure that StarMCLib supports this version of Minecraft");
+            plugin.getLogger().info("Incompatible version detected. Please ensure that " + plugin.getName() + " supports this version of Minecraft");
         }
         
         return UNDEFINED;
